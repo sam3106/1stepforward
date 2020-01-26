c1ec7ddd94953074572d3f226ee4b34889f6be8c
sam3106 <sampson2448@yahoo.com>
2020-01-26 10:19:15 -0500

    added new folders as well as the start of  building out the landing page animation

--- src/components/drop-down-menu/drop-down-menu.js
+++ src/components/drop-down-menu/drop-down-menu.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { DropDownWrapper, DropDownInnerContent } from "./drop-down-menu.style";
-// import {} from "./drop-down-menu.style.js"
+import {
+  DropDownWrapper,
+  DropDownInnerContent,
+  Left,
+  Bottom,
+  Right
+} from "./drop-down-menu.style";
 
 export const DropDownMenu = () => (
   <DropDownWrapper>
     <DropDownInnerContent>
-      <span>1 Step Forward is a accountablity app.Based off the crietria you set.</span>
-      <br/><span>Do you want to progress in the gym,use 1 step forward.</span>
-      <br/><span> Do you want to read more books to increase your knowlede, use 1 step forward. </span> 
-      <br/><span>Do you what ever your goals are it's time to take one step foward!</span>
+      <span>
+        1 Step Forward is a accountablity app.Based off the crietria you set.
+      </span>
+      <br />
+    </DropDownInnerContent>
+    <DropDownInnerContent>
+      <Left span="Left">
+        Do you want to progress in the gym,use 1 step forward.
+      </Left>
+      <br />
+    </DropDownInnerContent>
+    <DropDownInnerContent>
+      <Right span="Right">
+        Do you want to read more books to increase your knowlede, use 1 step
+        forward.
+      </Right>
+      <br />
+      <Bottom span="Bottom">
+        Do you what ever your goals are it's time to take one step foward!
+      </Bottom>
     </DropDownInnerContent>
   </DropDownWrapper>
 );
--- src/components/drop-down-menu/drop-down-menu.style.js
+++ src/components/drop-down-menu/drop-down-menu.style.js
@@ -1,24 +1,74 @@
 import styled from "styled-components";
 
+const SpanProps = span => {
+  switch (span) {
+    case "Left":
+      return "Left";
+      break;
+    case "Right":
+      return "Right";
+      break;
+    case "Bottom":
+      return "Bottom";
+      break;
+    default:
+      return null;
+  }
+};
+const Span = styled.span`
+  position: relative;
+  animation-name: ${({ span }) => SpanProps(span)};
+  animation-duration: 5s;
+ 
+`;
 export const DropDownWrapper = styled.div`
   width: 50%;
   margin: 0 auto;
+  postion:relative
+ 
 `;
 
 export const DropDownInnerContent = styled.p`
-font-size:26px;
+  font-size: 26px;
+`;
+export const Left = styled(Span)`
+  color: blue;
+  @keyframes Left {
+    from {
 
-
-`
-export const Left = styled.span`
-
-
-`
-export const Right = styled.span`
-
-
-`
-export const Bottom = styled.span`
-
-
-`
+      left: 200vw;
+      background-color: yellow;
+    }
+    to {
+     
+      left:0
+      background-color: blue;
+    }
+  }
+`;
+export const Right = styled(Span)`
+  color: red;
+  @keyframes Right {
+    from {
+      top: 100vh;
+      background-color: yellow;
+    }
+    to {
+      top: 0vh;
+    
+    }
+  }
+`;
+export const Bottom = styled(Span)`
+  color: green;
+  @keyframes Bottom {
+    from {
+      top: 100vh;
+      background-color: yellow;
+    }
+    to {
+      top: 0vh;
+      background-color: blue;
+    }
+  }
+`;

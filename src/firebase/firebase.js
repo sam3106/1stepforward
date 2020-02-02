ddcc5e2355faf98a4adfec0dc76bffec93354dab
sam3106 <sampson2448@yahoo.com>
2020-02-02 18:18:08 -0500

    Firebase added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+var firebaseConfig = {
+  apiKey: "AIzaSyCZ1Snss0T8n42n7cfiBLkFhnLdlEpvR54",
+  authDomain: "percentbetter-dad1b.firebaseapp.com",
+  databaseURL: "https://percentbetter-dad1b.firebaseio.com",
+  projectId: "percentbetter-dad1b",
+  storageBucket: "percentbetter-dad1b.appspot.com",
+  messagingSenderId: "669257682754",
+  appId: "1:669257682754:web:bdded90d4f543f577442db",
+  measurementId: "G-67HDBMTRND"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
